4db29814b2d8b8ca2e03db95ca9af388b1599d0b
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-25 19:02:57 +0300

    nest/ Creating the product's columns

--- nest/src/api/products/products.entity.ts
+++ nest/src/api/products/products.entity.ts
@@ -1,3 +1,16 @@
-export class Products {
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
+export class Products {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'varchar' })
+  public name!: string;
+
+  @Column({ type: 'varchar' })
+  public description: string;
+
+  @Column({ type: 'integer' })
+  public price: number;
 }
