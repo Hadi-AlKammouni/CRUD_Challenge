32704d4a0d64bd336830e83776a7471c6b7c22f0
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-25 19:04:06 +0300

    nest/ Adding createProduct service

--- nest/src/api/products/products.service.ts
+++ nest/src/api/products/products.service.ts
@@ -1,3 +1,18 @@
-export class ProductsService {
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Observable, from } from 'rxjs';
+import { Repository } from 'typeorm';
+import { Products } from './products.entity';
+import { ProductsPost } from './products.interface';
 
+@Injectable()
+export class ProductsService {
+    constructor(
+        @InjectRepository(Products)
+        private readonly productsPostRepository: Repository<Products>
+    ) {}
+
+    createProduct( productsPost: ProductsPost ):Observable<ProductsPost> {
+        return from(this.productsPostRepository.save(productsPost))
+    }
 }
