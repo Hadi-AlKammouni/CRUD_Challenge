87137f15678815792d03777eedf185dcd6fab84b
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-25 19:05:30 +0300

    nest/ Adding the post request to create a product in the products table

--- nest/src/api/products/products.controller.ts
+++ nest/src/api/products/products.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ProductsPost } from './products.interface';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+    constructor (private productsService: ProductsService) {}
 
+    @Post()
+    create(@Body() product: ProductsPost): Observable<ProductsPost> {
+        return this.productsService.createProduct(product)
+    }
 }
