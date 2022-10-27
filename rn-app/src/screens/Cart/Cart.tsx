c919f09ac1e106c0671f03509d1b4cf4342e404c
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-27 08:42:21 +0300

    rn/ Display products that are in the cart

--- rn-app/src/screens/Cart/Cart.tsx
+++ rn-app/src/screens/Cart/Cart.tsx
@@ -1,10 +1,45 @@
 import React from "react";
-import { View, SafeAreaView, TouchableOpacity, Image } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity, Image, FlatList } from "react-native";
 
+import { useCart } from "../../context/Cart";
 import styles from "./styles";
 
 const Cart = ({navigation}: {navigation: any}) => {
-    
+
+    const {cartItems, setCartItems} = useCart()
+
+    const Card = ({product}: {product: any}) => {
+        return (
+            <>
+                <View style={styles.card}>
+                    <View style={styles.product_img_view}>
+                        <Image style={styles.product_img} source={{uri: product.image}}/>
+                    </View>
+                    <Text style={styles.product_name}>{product.name}</Text>
+                    <View style={styles.product_details}>
+                        <Text style={styles.product_price}>${product.price}</Text>
+                        <View style={styles.quantity_view}>
+                            <View style={styles.btn_view}>
+                                <Text style={styles.add_remove_sign}>-</Text>
+                            </View>
+                            <Text style={styles.quantity}>1</Text>
+                            <View style={styles.btn_view}>
+                                <Text style={styles.add_remove_sign}>+</Text>
+                            </View>
+                        </View>
+                    </View>
+                        
+                    <TouchableOpacity onPress={() => setCartItems(cartItems.filter((item: { id: any; }) => item.id !== product.id))}>
+                        <View style={styles.remove_view}>
+                            <Text style={styles.sign}>Remove from cart</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                </View>
+            </>
+        )
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
@@ -16,6 +51,26 @@ const Cart = ({navigation}: {navigation: any}) => {
                     />
                 </TouchableOpacity>
             </View>
+            {cartItems.length === 0 ?
+                <View style={styles.title_header}>
+                        <Text style={styles.title}>Your didn't pick any product yet!</Text>
+                </View>
+            :
+                <>
+                <View style={styles.title_header}>
+                        <Text style={styles.title}>Your picked products!</Text>
+                </View>
+                <FlatList 
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        marginTop: 15,
+                        paddingBottom: 50
+                    }}
+                    data={cartItems} 
+                    renderItem={({item}) => <Card product={item}/>}
+                    />
+                </>
+            }
         </SafeAreaView>
     )
 }
--- rn-app/src/screens/Cart/styles.tsx
+++ rn-app/src/screens/Cart/styles.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
+        paddingHorizontal: 20,
         backgroundColor: "#fff"
     },
     header: {
@@ -14,7 +15,87 @@ const styles = StyleSheet.create({
     icon : {
         width: 40,
         height: 40,
-    }
+    },
+    title_header: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        backgroundColor: "#fff",
+        marginTop: 20,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+    card: {
+        height: 340,
+        backgroundColor: '#eaebec',
+        marginHorizontal: 2,
+        marginBottom: 20,
+        padding: 15,
+        borderRadius: 10,
+    },
+    product_img_view: {
+      height: 150,
+      alignItems: 'center'
+    },
+    product_img: {
+      flex: 1, 
+      height: 150,
+      width: 200,
+      resizeMode: 'contain',
+      borderRadius: 15
+    },
+    product_name: {
+      fontWeight: 'bold', 
+      fontSize: 17, 
+      marginTop:20
+    },
+    product_details: {
+      flexDirection: 'row', 
+      justifyContent:"space-between", 
+      alignItems: 'center',
+      marginTop: 5
+    },
+    product_price: {
+      fontSize: 19, 
+      fontWeight: 'bold'
+    },
+    remove_view: {
+      height: 40, 
+      marginTop: 20,
+      backgroundColor: '#FF0000', 
+      borderRadius: 15, 
+      justifyContent: 'center', 
+      alignItems: 'center',
+    },
+    sign: {
+      fontSize: 22, 
+      color: '#fff', 
+      fontWeight: 'bold'
+    },
+    add_remove_sign: {
+        fontSize: 22, 
+        fontWeight: 'bold'
+    },
+    quantity_view: {
+        flexDirection: 'row', 
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    btn_view: {
+        borderColor: '#5f96ec', 
+        borderWidth: 1, 
+        borderRadius: 5, 
+        height: 45, 
+        width: 60, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    },
+    quantity: {
+        fontSize: 20, 
+        marginHorizontal: 10, 
+        fontWeight: 'bold'
+    },
 });
 
 export default styles;
